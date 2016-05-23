33651f6d03be9189912a1725649dab2c14654b5d
totemtang <totem825@gmail.com>
2016-05-23 18:13:41 -0500

    opt train

--- benchmarks/single/single-train/single-train.go
+++ benchmarks/single/single-train/single-train.go
@@ -48,7 +48,6 @@ var nsecs = flag.Int("nsecs", 2, "number of seconds to run")
 var wl = flag.String("wl", "../single.txt", "workload to be used")
 var tp = flag.String("tp", "0:100", "Percetage of Each Transaction")
 var out = flag.String("out", "data.out", "output file path")
-var trainOut = flag.String("train", "train.out", "training set")
 var prof = flag.Bool("prof", false, "whether perform CPU profile")
 var sr = flag.Int("sr", 500, "Sample Rate")
 var tc = flag.String("tc", "train.conf", "configuration for training")
@@ -81,10 +80,6 @@ func main() {
 		clog.Error("WorkLoad not specified\n")
 	}
 
-	if strings.Compare(*trainOut, "") == 0 {
-		clog.Error("Training Output not specified\n")
-	}
-
 	if strings.Compare(*tc, "") == 0 {
 		clog.Error("Training Configuration not specified\n")
 	}
@@ -108,11 +103,16 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	f, err := os.OpenFile(*trainOut, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	fPart, err := os.OpenFile("train-part.out", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		clog.Error("Open File Error %s\n", err.Error())
 	}
-	defer f.Close()
+	defer fPart.Close()
+
+	fMerge, err1 := os.OpenFile("train-merge.out", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err1 != nil {
+		clog.Error("Open File Error %s\n", err1.Error())
+	}
 
 	occFile, err1 := os.OpenFile("occ.out", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err1 != nil {
@@ -157,6 +157,8 @@ func main() {
 	} else {
 		double = false
 	}
+
+	typeAR := make([]int, 0, LOCKSHARE+1)
 
 	clog.Info("Number of workers %v \n", nWorkers)
 	clog.Info("Adaptive CC Training %v \n", tm)
@@ -257,7 +259,7 @@ func main() {
 
 			for z := 0; z < TOTALCC; z++ { // Find the middle one
 				tmpFeature := ft[z]
-				for x := 0; x < 2; x++ {
+				for x := 0; x < 2; x++ { // Insert Sorting
 					tmp := tmpFeature[x]
 					tmpI := x
 					for y := x + 1; y < 3; y++ {
@@ -310,10 +312,25 @@ func main() {
 				}
 			}
 
-			for x := 0; x < 2; x++ { // Find the best one
+			// Sort the first three and last two respectively
+			for x := PCC; x < LOCKPART; x++ { // Find the best one
 				tmp := ft[x][1]
 				tmpI := x
-				for y := x + 1; y < TOTALCC; y++ {
+				for y := x + 1; y <= LOCKPART; y++ {
+					if tmp.Txn < ft[y][1].Txn {
+						tmp = ft[y][1]
+						tmpI = y
+					}
+				}
+				tmp = ft[x][1]
+				ft[x][1] = ft[tmpI][1]
+				ft[tmpI][1] = tmp
+			}
+
+			for x := OCCSHARE; x < LOCKSHARE; x++ { // Find the best one
+				tmp := ft[x][1]
+				tmpI := x
+				for y := x + 1; y <= LOCKSHARE; y++ {
 					if tmp.Txn < ft[y][1].Txn {
 						tmp = ft[y][1]
 						tmpI = y
@@ -325,7 +342,7 @@ func main() {
 			}
 
 			if tm != TESTING {
-				for z := 0; z < 3*TOTALCC; z++ {
+				for z := 0; z < 3*(LOCKPART+1); z++ {
 					x := z / 3
 					y := z % 3
 					if !(x == 0 && y == 1) {
@@ -333,25 +350,72 @@ func main() {
 					}
 				}
 
-				if tm == TRAINOCCPART || tm == TRAINOCCPURE {
+				if tm == TRAINOCCPART {
 					ft[0][1].Avg(float64(6))
-				} else if tm == TRAINPART {
+				} else if tm == TRAINPART || tm == TRAININDEX {
 					ft[0][1].Avg(float64(9))
-				} else if tm == TRAININDEX {
-					ft[0][1].Avg(float64(15))
+				}
+
+				for z := 3 * OCCSHARE; z < 3*(LOCKSHARE+1); z++ {
+					x := z / 3
+					y := z % 3
+					if !(x == OCCSHARE && y == 1) {
+						ft[OCCSHARE][1].Add(ft[x][y])
+					}
+				}
+
+				ft[OCCSHARE][1].Avg(float64(6))
+
+			}
+
+			// Find the train type with similar performance difference
+			if ft[0][1].Txn > ft[OCCSHARE][1].Txn {
+				typeNum := len(typeAR)
+				typeAR = typeAR[:typeNum+1]
+				typeAR[typeNum] = ft[0][1].TrainType
+				if (ft[0][1].Txn-ft[OCCSHARE][1].Txn)/ft[0][1].Txn < PERFDIFF {
+					typeNum := len(typeAR)
+					typeAR = typeAR[:typeNum+1]
+					typeAR[typeNum] = ft[OCCSHARE][1].TrainType
+				} else if (ft[0][1].Txn-ft[1][1].Txn)/ft[0][1].Txn < PERFDIFF {
+					typeNum := len(typeAR)
+					typeAR = typeAR[:typeNum+1]
+					typeAR[typeNum] = ft[1][1].TrainType
+				}
+			} else {
+				typeNum := len(typeAR)
+				typeAR = typeAR[:typeNum+1]
+				typeAR[typeNum] = ft[OCCSHARE][1].TrainType
+				if (ft[OCCSHARE][1].Txn-ft[0][1].Txn)/ft[OCCSHARE][1].Txn < PERFDIFF {
+					typeNum := len(typeAR)
+					typeAR = typeAR[:typeNum+1]
+					typeAR[typeNum] = ft[0][1].TrainType
+				} else if (ft[OCCSHARE][1].Txn-ft[OCCSHARE+1][1].Txn)/ft[OCCSHARE][1].Txn < PERFDIFF {
+					typeNum := len(typeAR)
+					typeAR = typeAR[:typeNum+1]
+					typeAR[typeNum] = ft[OCCSHARE+1][1].TrainType
 				}
 			}
 
-			// One Test Finished
-			f.WriteString(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t", count, curCR, tmpMP, tmpPS, tmpContention, tmpTlen, tmpRR))
-			if (ft[0][1].Txn-ft[1][1].Txn)/ft[0][1].Txn < PERFDIFF {
-				f.WriteString(fmt.Sprintf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%v\t%v\n", ft[0][1].PartConf, ft[0][1].PartVar, ft[0][1].RecAvg, ft[0][1].Latency, ft[0][1].ReadRate, ft[0][1].ConfRate, ft[0][1].TrainType, ft[1][1].TrainType))
-
-			} else {
-				f.WriteString(fmt.Sprintf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%v\n", ft[0][1].PartConf, ft[0][1].PartVar, ft[0][1].RecAvg, ft[0][1].Latency, ft[0][1].ReadRate, ft[0][1].ConfRate, ft[0][1].TrainType))
+			if tm == TRAINPART || tm == TRAINOCCPART || tm == TESTING || tm == TRAININDEX {
+				fPart.WriteString(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t", count, curCR, tmpMP, tmpPS, tmpContention, tmpTlen, tmpRR))
+				fPart.WriteString(fmt.Sprintf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", ft[0][1].PartConf, ft[0][1].PartVar, ft[0][1].RecAvg, ft[0][1].Latency, ft[0][1].ReadRate, ft[0][1].ConfRate))
+				for _, trainType := range typeAR {
+					fPart.WriteString(fmt.Sprintf("\t%v", trainType))
+				}
+				fPart.WriteString("\n")
 			}
 
-			win := ft[0][1].Mode
+			if tm == TRAINOCCPURE || tm == TESTING || tm == TRAININDEX {
+				fMerge.WriteString(fmt.Sprintf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t", count, curCR, tmpMP, tmpPS, tmpContention, tmpTlen, tmpRR))
+				fMerge.WriteString(fmt.Sprintf("%.4f\t%.4f\t%.4f\t%.4f\t%.4f\t%.4f", ft[OCCSHARE][1].PartConf, ft[OCCSHARE][1].PartVar, ft[OCCSHARE][1].RecAvg, ft[OCCSHARE][1].Latency, ft[OCCSHARE][1].ReadRate, ft[OCCSHARE][1].ConfRate))
+				for _, trainType := range typeAR {
+					fMerge.WriteString(fmt.Sprintf("\t%v", trainType))
+				}
+				fMerge.WriteString("\n")
+			}
+
+			win := typeAR[0]
 
 			// One Test Finished
 			for _, features := range ft {
@@ -448,6 +512,7 @@ func oneTest(single *testbed.SingelWorkload, coord *testbed.Coordinator, ft [][]
 								t.SetTID(tid)
 							}
 						}
+
 						if t.GetTXN() != -1 {
 							w.NGen += time.Since(tm)
 						}
